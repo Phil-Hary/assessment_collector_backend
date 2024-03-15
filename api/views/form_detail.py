f72c67b324f93d8758e44b951f5c1fa5e7a267a7
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-15 13:21:34 +0530

    [ FEAT ] - update config

--- api/views/form_detail.py
+++ api/views/form_detail.py
@@ -8,7 +8,7 @@ from api.models import Form
 from api.serializers import FormConfigPayloadSerializer
 
 class FormDetailViewSet(ViewSet):
-    @action(detail=False, methods=['GET'], url_path="config")
+    @action(detail=False, methods=['GET', "POST"], url_path="config")
     @response_handler
     def get_config(self, request, form_name):
         form = None
@@ -16,28 +16,26 @@ class FormDetailViewSet(ViewSet):
         try:
             form = Form.objects.get(slug=form_name)
         except Form.DoesNotExist:
-            return APIResponse(message="Form does not exists", status_code=status.HTTP_400_BAD_REQUEST)
+            return APIResponse(message="Form does not exists", status_code=status.HTTP_404_NOT_FOUND)
+        
+        if request.method == "GET":
+            return APIResponse(data=form.config, status_code=status.HTTP_200_OK)
+        
+        elif request.method == "POST":
+            serialized_payload = FormConfigPayloadSerializer(data=request.data)
 
-        return APIResponse(data=form.config, status_code=status.HTTP_200_OK)
+            if not serialized_payload.is_valid():
+                return APIResponse(message=f"Invalid config {serialized_payload.errors}", status_code=status.HTTP_400_BAD_REQUEST)
+
+            response = serialized_payload.data
+
+            try:
+                form.config = response
+                form.save()
+            except Exception:
+                return APIResponse(message="An error occurred while saving the config", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            return APIResponse(message="Form config updated successfully", status_code=status.HTTP_200_OK)
 
 
-    @action(detail=False, methods=['post'], url_path="save_config")
-    @response_handler
-    def update_config(self, request, form_name):
-        form = None
-
-        serialized_payload = FormConfigPayloadSerializer(data=request.data)
-
-        if not serialized_payload.is_valid():
-            return APIResponse(message=f"Invalid response {serialized_payload.errors}", status_code=status.HTTP_400_BAD_REQUEST)
-
-        response = serialized_payload.data
-
-        try:
-            form = Form.objects.get(slug=form_name)
-            form.config = response
-            form.save()
-        except Form.DoesNotExist:
-            return APIResponse(message="Form does not exists", status_code=status.HTTP_400_BAD_REQUEST)
-
-        return APIResponse(message="Form config updated successfully", status_code=status.HTTP_200_OK)
+        
